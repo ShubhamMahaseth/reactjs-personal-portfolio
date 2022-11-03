dea75b90047314cd1bc8cc12155661028cd1b98d
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-11-03 18:59:12 +0530

    changes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer>
       <a href="#" className="footer__logo">
-        PERSONAL PROFI
+        PERSONAL PROFILE
       </a>
       <ul className="permalinks">
         <li>
